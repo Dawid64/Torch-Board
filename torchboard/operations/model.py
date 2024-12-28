4ce4e7554cb7b51fbca8788e5c515c9b6dc4d929
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2024-12-28 23:03:32 +0100

    add hooks for layers

--- torchboard/operations/model.py
+++ torchboard/operations/model.py
@@ -1,4 +1,51 @@
 from torch.nn import Module
+from torch import nn
+import torch
+import numpy as np
+import torchvision
+from matplotlib import pyplot as plt
+import matplotlib
+
+matplotlib.use("Agg")
+
+
 class ModelOperator:
-    def __init__(self, model:Module):
+    def __init__(self, model: Module):
         self.model = model
+        self.cache = None
+
+    def get_current_device(self):
+        return next(iter(self.model.parameters())).device
+
+    def get_layers(self):
+        modules = []
+        for name, module in self.model.named_children():
+            modules.append((name, module))
+        return modules
+
+    def get_parameters(self):
+        params = []
+        for name, param in self.model.named_parameters():
+            params.append((name, param))
+        return params
+
+    def get_module_by_name(self, module_name: str):
+        return getattr(self.model, module_name)
+
+    def register_forward_hooks(self, module_name: str, output_num: int = 0):
+        module = self.get_module_by_name(module_name)
+
+        def forward_hook(module, input, output):
+            self.cache = output[output_num]
+            handle.remove()
+
+        handle = module.register_forward_hook(forward_hook)
+
+    def register_backward_hooks(self, module_name: str, output_num: int = 0):
+        module = self.get_module_by_name(module_name)
+
+        def backward_hook(module, grad_input, grad_output):
+            self.cache = grad_output[output_num]
+            handle.remove()
+
+        handle = module.register_backward_hook(backward_hook)
--- torchboard/server/torchboard_server.py
+++ torchboard/server/torchboard_server.py
@@ -12,109 +12,155 @@ from torchboard.server.utils import wipe_dir, transform_history_dict
 
 from typing import Any, List
 import logging
-log = logging.getLogger('werkzeug')
+
+log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
-class TorchBoardServer():
-    port:int
-    host:str
-    static_path:str
-    
-    listener_state:dict[str, List[Any]]
-    variable_state:dict[str, Any]
-    
-    app:flask.Flask
-    
-    def __init__(self, port:int=8080, host:str='127.0.0.1', name:str='TorchBoard', static_path:str='static', board:Any=None) -> None:
+
+class TorchBoardServer:
+    port: int
+    host: str
+    static_path: str
+
+    listener_state: dict[str, List[Any]]
+    variable_state: dict[str, Any]
+
+    app: flask.Flask
+
+    def __init__(
+        self,
+        port: int = 8080,
+        host: str = "127.0.0.1",
+        name: str = "TorchBoard",
+        static_path: str = "static",
+        board: Any = None,
+    ) -> None:
         self.port = port
         self.host = host
-        self.static_path = static_path        
-        
+        self.static_path = static_path
+
         self.variable_state = dict()
-        
+
         self.app = flask.Flask(name)
         self.__flask_process = None
-        
-        self.app.config["SECRET_KEY"] = "duparomana123" #TODO: Change secret key
-        self.app.config['SESSION_TYPE'] = 'filesystem'
-        
-        CORS(self.app, resources={r"/*": {"origins": "*"}}) #TODO: Change origins to specific domain
+
+        self.app.config["SECRET_KEY"] = "duparomana123"  # TODO: Change secret key
+        self.app.config["SESSION_TYPE"] = "filesystem"
+
+        CORS(
+            self.app, resources={r"/*": {"origins": "*"}}
+        )  # TODO: Change origins to specific domain
         Session(self.app)
         self.board = board
 
-        @self.app.route('/')
+        @self.app.route("/")
         def index():
-            return flask.send_from_directory(self.static_path, 'index.html')
-        
-        @self.app.route('/<path:path>')
+            return flask.send_from_directory(self.static_path, "index.html")
+
+        @self.app.route("/<path:path>")
         def static_proxy(path):
             return flask.send_from_directory(self.static_path, path)
-        
-        self.app.add_url_rule('/get_changes','get_changes', self.__get_changes_session, methods=['GET'])
-        self.app.add_url_rule('/get_history','get_history', self.__get_history, methods=['GET'])
-        self.app.add_url_rule('/get_variables','get_variables', self.__get_variables, methods=['GET'])
-        self.app.add_url_rule('/update_variable','update_variable', self.__update_variable, methods=['PUT'])
-        
+
+        self.app.add_url_rule(
+            "/get_changes", "get_changes", self.__get_changes_session, methods=["GET"]
+        )
+        self.app.add_url_rule(
+            "/get_history", "get_history", self.__get_history, methods=["GET"]
+        )
+        self.app.add_url_rule(
+            "/get_variables", "get_variables", self.__get_variables, methods=["GET"]
+        )
+        self.app.add_url_rule(
+            "/update_variable",
+            "update_variable",
+            self.__update_variable,
+            methods=["PUT"],
+        )
+
         self.server = make_server(self.host, self.port, self.app, threaded=True)
         self.server.daemon_threads = True
-    
-    
+
     def start(self, start_browser=False) -> None:
         if self.__flask_process:
             return
         self.__flask_process = Thread(target=self.server.serve_forever)
         self.__flask_process.daemon = True
         self.__flask_process.start()
-        
-        if os.path.exists('flask_session'):
-            wipe_dir('flask_session')
-        
+
+        if os.path.exists("flask_session"):
+            wipe_dir("flask_session")
+
         if start_browser:
-            webbrowser.open(f'http://{self.host}:{self.port}') #Force open browser to dashboard
-    
+            webbrowser.open(
+                f"http://{self.host}:{self.port}"
+            )  # Force open browser to dashboard
+
     def stop(self) -> None:
         self.__flask_process.join()
         self.__flask_process = None
-    
+
     @cross_origin()
     def __get_changes_session(self) -> flask.Response:
         changes_list = self.board.history.get_since_last_change()
-        return flask.jsonify(transform_history_dict(changes_list)),200
+        return flask.jsonify(transform_history_dict(changes_list)), 200
+
+    @cross_origin()
+    def __get_layers(self) -> flask.Response:
+        layers = self.board.model.get_layers()
+        return flask.jsonify(layers), 200
+
+    @cross_origin()
+    def __setup_output_hook(self, module_name: str, output_num: int = 0):
+        self.board.model.register_forward_hooks(module_name, output_num)
+        return flask.jsonify({"status": "success"}), 200
+
+    @cross_origin()
+    def __return_cached_output(self) -> flask.Response:
+        if self.board.model.cache is None:
+            return flask.jsonify({"status": "error", "message": "No imaged yet"}), 404
+        else:
+            output = self.board.model.cache
+            self.board.model.cache = None
+            return flask.jsonify(output), 200
 
     @cross_origin()
     def __get_history(self) -> flask.Response:
         history = self.board.history.get_all()
         return flask.jsonify(history), 200
-    
+
     @cross_origin()
     def __get_variables(self) -> flask.Response:
-        return flask.jsonify({key:value for key,value in self.variable_state.items()}),200
-    
+        return flask.jsonify(
+            {key: value for key, value in self.variable_state.items()}
+        ), 200
+
     @cross_origin()
     def __update_variable(self) -> flask.Response:
         data = flask.request.json
-        if any([key not in data for key in ['name','value']]):
-            return flask.jsonify({'status': 'error', 'message': 'Invalid request'}),400
-            
-        name,value = data['name'],data['value']
-        
+        if any([key not in data for key in ["name", "value"]]):
+            return flask.jsonify({"status": "error", "message": "Invalid request"}), 400
+
+        name, value = data["name"], data["value"]
+
         if not name in self.variable_state:
-            return flask.jsonify({'status': 'error', 'message': f'Variable {name} not found'}),404
-            
+            return flask.jsonify(
+                {"status": "error", "message": f"Variable {name} not found"}
+            ), 404
+
         self.update_changeable_value(name, value)
-        return flask.jsonify({'status': 'success'}),200
-        
-    def register_changeable_value(self,name:str,default_value:Any) -> None:
+        return flask.jsonify({"status": "success"}), 200
+
+    def register_changeable_value(self, name: str, default_value: Any) -> None:
         self.variable_state[name] = default_value
-        
-    def update_changeable_value(self,name:str,value:Any) -> None:
+
+    def update_changeable_value(self, name: str, value: Any) -> None:
         self.variable_state[name] = value
-        
-    def get_changeable_value(self,name:str) -> Any | None:
+
+    def get_changeable_value(self, name: str) -> Any | None:
         if name in self.variable_state:
             return self.variable_state[name]
         else:
             return None
-    
+
     def get_changeable_values(self) -> dict[str, Any]:
-        return {k:v for k,v in self.variable_state.items()}
+        return {k: v for k, v in self.variable_state.items()}
