ca17a20fd1231d169eb89fb6886cb22641bffa19
Jacob Millet <jakub.jagla01@gmail.com>
2024-12-28 16:34:17 +0100

    multiprocessing variables

--- torchboard/server/torchboard_server.py
+++ torchboard/server/torchboard_server.py
@@ -2,26 +2,32 @@ import flask
 from flask_cors import CORS, cross_origin
 from flask_session import Session
 
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 
 import webbrowser
 
-from torchboard.base.listener import Listener
-
 from typing import Any, List
+
+import os
+from torchboard.server.utils import wipe_dir  
 class TorchBoardServer():
     port:int
     host:str
     static_path:str
-    listener_state:dict[str, Listener]
-    changes:dict[str, list]
+    
+    listener_state:dict[str, List[Any]]
+    variable_state:dict[str, Any]
+    
     app:flask.Flask
     
     def __init__(self, port:int=8080, host:str='127.0.0.1', name:str='TorchBoard', static_path:str='static') -> None:
         self.port = port
         self.host = host
         
-        self.listener_state = {}
+        self.__mp_manager = Manager()
+        
+        self.listener_state = self.__mp_manager.dict()
+        self.variable_state = self.__mp_manager.dict()
         
         self.app = flask.Flask(name)
         self.__flask_process = None
@@ -42,17 +48,19 @@ class TorchBoardServer():
         
         self.app.add_url_rule('/get_changes','get_changes', self.__get_changes_session, methods=['GET'])
         self.app.add_url_rule('/get_history','get_history', self.__get_history, methods=['GET'])
+        self.app.add_url_rule('/get_variables','get_variables', self.__get_variables, methods=['GET'])
+        self.app.add_url_rule('/update_variable','update_variable', self.__update_variable, methods=['PUT'])
         
     @cross_origin()
     def __get_changes_session(self) -> flask.Response:
         history_indexes = flask.session.get('history_indexes', dict())
         changes = dict()
-        for key,listener in self.listener_state.items():
+        for key,history in self.listener_state.items():
             idx = history_indexes.get(key, 0)
-            updates = listener.history[idx:]
+            updates = history[idx:]
             if updates:
                 changes[key] = updates
-                history_indexes[key] = len(listener.history)
+                history_indexes[key] = len(history)
         flask.session['history_indexes'] = history_indexes
         return flask.jsonify(changes),200
 
@@ -60,11 +68,31 @@ class TorchBoardServer():
     def __get_history(self) -> flask.Response:
         ret = dict()
         for key in self.listener_state:
-            ret[key] = self.listener_state[key].get_all()
+            ret[key] = self.listener_state[key]
             if not 'history_indexes' in flask.session:
                 flask.session['history_indexes'] = dict()
-            flask.session['history_indexes'][key] = len(self.listener_state[key].history)
+            flask.session['history_indexes'][key] = len(self.listener_state[key])
         return flask.jsonify(ret),200
+    
+    @cross_origin()
+    def __get_variables(self) -> flask.Response:
+        return flask.jsonify({key:value for key,value in self.variable_state.items()}),200
+    
+    @cross_origin()
+    def __update_variable(self) -> flask.Response:
+        data = flask.request.json
+        
+        if not 'name' in data or not 'value' in data:
+            return flask.jsonify({'status': 'error', 'message': 'Invalid request'}),400
+        
+        name = data['name']
+        value = data['value']
+        
+        if not name in self.variable_state:
+            return flask.jsonify({'status': 'error', 'message': f'Variable {name} not found'}),404
+            
+        self.update_variable(name, value)
+        return flask.jsonify({'status': 'success'}),200
 
     def __run_flask(self) -> None:
         self.app.run(port=self.port, host=self.host)
@@ -74,17 +102,20 @@ class TorchBoardServer():
             return
         self.__flask_process = Process(target=self.__run_flask, name=self.app.name) #Non blocking
         self.__flask_process.start()
-        #webbrowser.open(f'http://{self.host}:{self.port}') #Force open browser to dashboard
+        webbrowser.open(f'http://{self.host}:{self.port}') #Force open browser to dashboard
     
     def stop(self) -> None:
         self.__flask_process.terminate()
         self.__flask_process.join()
         self.__flask_process = None
+        wipe_dir('flask_session') 
+        os.rmdir('flask_session') 
+        
     
     def add_listener_variable(self, name:str, value:Any) -> None:
         if name not in self.listener_state:
-            self.listener_state[name] = Listener()
-        self.listener_state[name].add(value)
+            self.listener_state[name] = self.__mp_manager.list()
+        self.listener_state[name].append(value)
     
     def add_listener_variables(self, variables:dict[str, Any]) -> None:
         for key, value in variables.items():
@@ -92,13 +123,34 @@ class TorchBoardServer():
     
     def get_listener_variable_history(self, name:str) -> List[Any] | None:
         if name in self.listener_state:
-            return self.listener_state[name].get_all()
+            return self.listener_state[name]
         else:
             return None        
-    
         
+    def register_variable(self,name:str,default_value:Any) -> None:
+        self.variable_state[name] = default_value
+        
+    def update_variable(self,name:str,value:Any) -> None:
+        self.variable_state[name] = value
+        
+    def get_variable(self,name:str) -> Any | None:
+        if name in self.variable_state:
+            return self.variable_state[name]
+        else:
+            return None
+    
+    def get_variables(self) -> dict[str, Any]:
+        return self.variable_state
         
 if __name__ == '__main__':
     server = TorchBoardServer(static_path='../../static')
     server.add_listener_variables({'test': 1, 'test2': 2})
+    server.register_variable('test', 1)
     server.run()
+    try:
+        #this is stupid
+        while True:
+            pass    
+    except KeyboardInterrupt:
+        server.stop()
+    
